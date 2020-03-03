91e95a2e377ee8e1745a31a038710daeaa44d6a9
melhajjami <melhajjami8@boscdelacoma.cat>
2020-03-03 17:02:02 +0100

    Commit 2

--- Main.gd
+++ Main.gd
@@ -1,7 +1,9 @@
 extends Node
 
 export (PackedScene) var Mob
+export (PackedScene) var MobDeurat 
 var score
+var mobDeuratSumar = false
 
 func _ready():
 	randomize()
@@ -31,25 +33,50 @@ func new_game():
 
 
 func _on_MobTimer_timeout():
-	# Choose a random location on Path2D.
-	$MobPath/MobSpawnLocation.set_offset(randi())
-	# Create a Mob instance and add it to the scene.
-	var mob = Mob.instance()
-	add_child(mob)
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
-	# Set the mob's position to a random location.
-	mob.position = $MobPath/MobSpawnLocation.position
-	# Add some randomness to the direction.
-	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-	# Set the velocity (speed & direction).
-	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
-	mob.linear_velocity = mob.linear_velocity.rotated(direction)
-	$HUD.connect("start_game", mob, "_on_start_game")
+	var percent = randf()
+	if (percent < 0.8):
+		# Choose a random location on Path2D.
+		$MobPath/MobSpawnLocation.set_offset(randi())
+		# Create a Mob instance and add it to the scene.
+		var mob = Mob.instance()
+		add_child(mob)
+		# Set the mob's direction perpendicular to the path direction.
+		var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+		# Set the mob's position to a random location.
+		mob.position = $MobPath/MobSpawnLocation.position
+		# Add some randomness to the direction.
+		direction += rand_range(-PI / 4, PI / 4)
+		mob.rotation = direction
+		# Set the velocity (speed & direction).
+		mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+		mob.linear_velocity = mob.linear_velocity.rotated(direction)
+		$HUD.connect("start_game", mob, "_on_start_game")
+	else:
+		mobDeuratSumar = true
+		# Choose a random location on Path2D.
+		$MobPath/MobSpawnLocation.set_offset(randi())
+		# Create a Mob instance and add it to the scene.
+		var mob = MobDeurat.instance()
+		add_child(mob)
+		# Set the mob's direction perpendicular to the path direction.
+		var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+		# Set the mob's position to a random location.
+		mob.position = $MobPath/MobSpawnLocation.position
+		# Add some randomness to the direction.
+		direction += rand_range(-PI / 4, PI / 4)
+		mob.rotation = direction
+		# Set the velocity (speed & direction).
+		mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+		mob.linear_velocity = mob.linear_velocity.rotated(direction)
+		$HUD.connect("start_game", mob, "_on_start_game")
+	
 
 func _on_ScoreTimer_timeout():
-	score += 1
+	if(mobDeuratSumar == false):
+		score += 1
+	else:
+		score += 2
+	mobDeuratSumar = false
 	$HUD.update_score(score)
 
 
--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 export var min_speed = 150  # Minimum speed range.
 export var max_speed = 250  # Maximum speed range.
-var mob_types = ["walk", "swim", "fly"]
+var mob_types = ["walk"]
 
 
 # Declare member variables here. Examples:
@@ -12,7 +12,7 @@ var mob_types = ["walk", "swim", "fly"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	$AnimatedSprite.animation = mob_types[0]
 
 func _on_Visibility_screen_exited():
 	queue_free()
